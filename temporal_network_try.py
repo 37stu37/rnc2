3a7eb9cdcaf64f2a67635b799c45165b69d430f2
37stu37 <60672071+37stu37@users.noreply.github.com>
2020-05-13 11:45:32 +1200

    Update temporal_network_try.py

--- temporal_network_try.py
+++ temporal_network_try.py
@@ -30,17 +30,14 @@ wind = data_folder / 'Copy of GD_wind.csv '
 edges = pd.read_parquet(edge, engine='pyarrow')
 
 #%%
-
-
-@jit
-def wind_scenario(file_name): # wind characteristics, bearing and distance
-    # wind scenario conditions
-    wind_data = pd.read_csv(file_name)
+@jit(nopython=True, parallel=True)
+def wind_scenario(file_name): 
+    wind_data = pd.read_csv(file_name) 
     i = np.random.randint(0, wind_data.shape[0])
     w = wind_data.iloc[i, 2]
     dist = wind_data.iloc[i, 1]
     b = wind_data.iloc[i, 3]
-    bear_max = b + 45 # wind direction
+    bear_max = b + 45  # wind direction
     bear_min = b - 45
     if b == 360:
         bear_max = 45
@@ -49,13 +46,11 @@ def wind_scenario(file_name): # wind characteristics, bearing and distance
     if b == 999:
         bear_max = 999
         bear_min = 0
-    return bear_max, bear_min, wind_distance
-    
-    return bear_max, bear_min, dist
+    return bear_max, bear_min, dist # wind characteristics, bearing and distance
 
 
-def display_network(edge_list_dataframe): # display edge list as network
-    graph = nx.from_pandas_edgelist(edge_list_dataframe, edge_attr=True)
+def display_network(edge_list_dataframe): 
+    graph = nx.from_pandas_edgelist(edge_list_dataframe, edge_attr=True) # display edge list as network
     options = {'node_color': 'red', 'node_size': 50, 'width': 1, 'alpha': 0.4,
                'with_labels': False, 'font_weight': 'bold'}
     nx.draw_kamada_kawai(graph, **options)
@@ -63,42 +58,49 @@ def display_network(edge_list_dataframe): # display edge list as network
     return graph
 
 
-@jit
-def create_contact_array(edgelist): # create contact array on the same index as edge list
-    return np.full((edgelist.values.shape[0],1), True)
+@jit(nopython=True, parallel=True)
+def create_contact_array(edgelist): 
+    return np.full((edgelist.values.shape[0],1), True) # create contact array on the same index as edge list
 
 
-@jit
-def initial_conditions(edgelist): # conditions, return a boolean
+@jit(nopython=True, parallel=True)
+def initial_conditions(edgelist): 
     rng = np.random.uniform(0, 1, size=edgelist.values.shape[0])
-    boolean = rng < edgelist.IgnProb_bl.values
-    return boolean
+    return rng < edgelist.IgnProb_bl.values  # conditions, return a boolean
 
-@jit
-def propagation_conditions(edgelist, contact_array, bear_max, bear_min, dist): # conditions, return a boolean
+
+@jit(nopython=True, parallel=True)
+def propagation_conditions(edgelist, contact_array, bear_max, 
+                           bear_min, dist):
     boolean1 = (edgelist.distance.values < dist) 
     boolean2 = (edgelist.bearing < bear_max) and (edgelist.bearing > bear_min)
     boolean3 = np.any(contact_array == True, axis=1) # columns where any elements are True ~ is it already burnt ?
-    # create boolean mask for all conditions
+    return boolean1 & boolean2 & boolean3# create boolean mask for all conditions
     return boolean1 & boolean2 & boolean3
     
 
-@jit
+@jit(nopython=True, parallel=True)
 def update_contacts(contact_array, boolean_array): # update a new contact time column
     return np.c_[contact_array, boolean_array]
-    
+   
 
-@jit
+@jit(nopython=True, parallel=True)
+
 def filter_edgelist_at_time(edgelist, contact_array, time): # new edges list at time
     return edgelist.values[contact_array[:, time] == True]
 
-@jit
+
+@jit(nopython=True, parallel=True)
 def main(e, n_scenario):
     for scenario in range(n_scenario):
-        contact_array = create_contact_array(edges)
-        wind_bearing_max, wind_bearing_min, wind_distance = wind_scenario(wind)
-        ignition_bool = initial_conditions(edges)
-        contact_array = update_contacts(contact_array, ignition_bool)
+        time = 0
+        wind_bearing_max, wind_bearing_min, wind_distance = wind_scenario(wind) # wind conditions
+        contact_array = create_contact_array(edges)  # create contact array
+        ignition_bool = initial_conditions(edges)  # set ignition conditions
+        contact_array = update_contacts(contact_array, ignition_bool)  # set edgelist based on ignition condtions mask
+        while (True in contact_array[:, -1]):
+            time += 1
+            
         
     
     
